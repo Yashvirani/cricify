63efd28fba8e8f38952b9d9c5329225bb8800d68
Yash Virani <62023226+Yashvirani@users.noreply.github.com>
2022-12-14 12:23:46 +0530

    Update uploadRouter.js

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,22 +1,69 @@
-import multer from 'multer';
 import express from 'express';
-import { isAuth } from '../utils.js';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+import config from '../config.js';
+import { v4 as uuidv4 } from 'uuid';
 
-const uploadRouter = express.Router();
+const s3 = new aws.S3({
+  accessKeyId: config.ACCESS_KEY_ID,
+  secretAccessKey: config.SECRET_ACCESS_KEY,
+  region: config.AWS_REGION,
+});
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`);
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, '');
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage }).single('image');
 
-uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`);
+const uploadrouter = express.Router();
+
+uploadrouter.post('/s3', upload, (req, res) => {
+  let file = req.file.originalname.split('.');
+  const filetype = file[file.length - 1];
+  console.log(req.file);
+
+  const params = {
+    Bucket: config.AWS_BUCKET,
+    Acl: 'public-read',
+    Key: `${uuidv4()}.${filetype}`,
+    Body: req.file.buffer,
+  };
+
+  s3.upload(params, (error, data) => {
+    console.log('error' + error);
+    if (error) {
+      res.status(500).send(error);
+    }
+
+    res.status(200).send(data);
+  });
 });
 
-export default uploadRouter;
+export default uploadrouter;
+
+// import multer from 'multer';
+// import express from 'express';
+// import { isAuth } from '../utils.js';
+
+// const uploadRouter = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename(req, file, cb) {
+//     cb(null, `${Date.now()}.jpg`);
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
+//   res.send(`/${req.file.path}`);
+// });
+
+// export default uploadRouter;
